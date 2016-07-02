f9c9d65c0c71c26ad9c56c737cd7ef09bb32fae5
Karen <karenjxl@gmail.com>
2016-07-02 13:32:43 -0500

    Completed exercise, but my code is a bit flawed. The reversed string deletes any repeat characters. So if the string is hello, it returns oleh.

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -3,7 +3,17 @@
 # comes with Ruby!
 
 def reverse_a_string(string)
-
+  input_array = string.chars
+  new_array = []
+  until input_array == []
+    input_array.each do |character|
+      if input_array.index(character) == input_array.index(input_array[-1])
+        new_array << input_array[input_array.index(character)]
+        input_array.delete(input_array[-1])
+      end
+    end
+  end
+  reversed_string = new_array.join
 end
 
 # Driver code - don't touch anything below this line.
