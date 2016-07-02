fb96fff805b11d6a62feb5bc27a64f1e4f242e25
Karen <karenjxl@gmail.com>
2016-07-02 13:20:44 -0500

    so weird- it gets rid of repeated letters. I think I know why.

--- algo/testing.rb
+++ algo/testing.rb
@@ -1,13 +1,28 @@
 def reverse_a_string(string)
-  string_array = string.chars
-  p string_array
-  string_hash = Hash.new
-  string_array.each do |character|
-    string_hash[character] = string_array.index(character)
+  input_array = string.chars
+  p input_array
+  puts
+  puts
+  new_array = []
+  until input_array == []
+    input_array.each do |character|
+      if input_array.index(character) == input_array.index(input_array[-1])
+        new_array << character
+        p new_array
+        puts
+        puts
+        input_array.delete(input_array[-1])
+        p input_array
+      end
+    end
   end
-  p string_hash
-  string_hash = string_hash.sort_by{ |_key, value| value }.reverse.to_h
-  puts string_hash.keys
+  puts
+  puts
+  p new_array
 end
 
 reverse_a_string("Hello")
+
+reverse_a_string("spaghetti")
+
+reverse_a_string("James")
