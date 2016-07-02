526582d6dba3b2aed691dcd3f9f7a11712ef8f0d
Karen <karenjxl@gmail.com>
2016-07-02 13:29:27 -0500

    I didn't fix the deletion-of-repeated-character problem, but it works.

--- algo/testing.rb
+++ algo/testing.rb
@@ -1,28 +1,16 @@
 def reverse_a_string(string)
   input_array = string.chars
-  p input_array
-  puts
-  puts
   new_array = []
   until input_array == []
     input_array.each do |character|
       if input_array.index(character) == input_array.index(input_array[-1])
-        new_array << character
-        p new_array
-        puts
-        puts
+        new_array << input_array[input_array.index(character)]
         input_array.delete(input_array[-1])
-        p input_array
       end
     end
   end
-  puts
-  puts
-  p new_array
+  reversed_string = new_array.join
+  puts reversed_string
 end
 
 reverse_a_string("Hello")
-
-reverse_a_string("spaghetti")
-
-reverse_a_string("James")
