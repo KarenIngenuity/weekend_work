914693ffb7e01ba9d4db72c8e66254406e8c3a37
Karen <karenjxl@gmail.com>
2016-07-02 13:39:27 -0500

    Cleaned up the code a little bit by using the .last method

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -7,9 +7,9 @@ def reverse_a_string(string)
   new_array = []
   until input_array == []
     input_array.each do |character|
-      if input_array.index(character) == input_array.index(input_array[-1])
-        new_array << input_array[input_array.index(character)]
-        input_array.delete(input_array[-1])
+      if character == input_array.last
+        new_array << character
+        input_array.delete(input_array.last)
       end
     end
   end
