8828cff0d83b46dd151642d2f013de19dded765d
Karen <karenjxl@gmail.com>
2016-07-02 13:07:39 -0500

    trying reverse_a_string with hash reverse

--- algo/testing.rb
+++ algo/testing.rb
@@ -1,6 +1,13 @@
 def reverse_a_string(string)
   string_array = string.chars
   p string_array
+  string_hash = Hash.new
+  string_array.each do |character|
+    string_hash[character] = string_array.index(character)
+  end
+  p string_hash
+  string_hash = string_hash.sort_by{ |_key, value| value }.reverse.to_h
+  puts string_hash.keys
 end
 
-reverse_a_string("Hello dear brother, what have you got there?")
+reverse_a_string("Hello")
